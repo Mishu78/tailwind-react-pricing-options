c8580f5c90cafd842d2a115a984a019adf93df5a
Mishu78 <meherun2002@gmail.com>
2026-03-29 15:15:12 +0600

    barchart added

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ import NavBar from './Components/NavBar/NavBar'
 import Link from './Components/NavBar/NavBar'
 import PricingOptions from './Components/PricingOptions/PricingOptions'
 import ResultsChart from './Components/ResultsChart/ResultsChart'
+import axios from 'axios'
+import MarksChart from './Components/MarksChart/MarksChart'
 
 const pricingPromise=fetch('pricingData.json')
 .then(res=>res.json());
+
+const markspromise=axios.get('marksData.json');
 
 function App() {
   return (
@@ -22,6 +26,9 @@ function App() {
         <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
       </Suspense>
         
+      <Suspense fallback={<span class="loading loading-spinner loading-lg"></span>}>
+        <MarksChart marksPromise={markspromise}></MarksChart>
+      </Suspense>
         <ResultsChart></ResultsChart>
       </main>
    
---
+++ src/Components/MarksChart/MarksChart.jsx
@@ -0,0 +1,36 @@
+import React, { use } from 'react';
+import { Bar, BarChart, XAxis, YAxis } from 'recharts';
+
+const MarksChart = ({marksPromise}) => {
+    const marksDataRes= use (marksPromise);
+    const marksData=marksDataRes.data;
+
+    const marksChartData=marksData.map(studentData=>{
+        const student={
+            id:studentData.student_id,
+            name:studentData.name,
+            physics:studentData.marks.physics,
+            chemistry:studentData.marks.chemistry,
+            math:studentData.marks.math
+        }
+      
+   
+    //console.log(marksChartData)
+    const avg=(student.physics+student.chemistry+student.math)/3;
+    student.avg=avg;
+    return student;
+     })
+    return (
+        <div>
+            <BarChart width={500} height={300} data={marksChartData}>
+                <XAxis dataKey='name'></XAxis>
+                <YAxis></YAxis>
+                <Bar dataKey='avg' fill='yellow'></Bar>
+                <Bar dataKey='chemistry' fill='blue'></Bar>
+                <Bar dataKey='physics' fill='red'></Bar>
+            </BarChart>
+        </div>
+    );
+};
+
+export default MarksChart;
